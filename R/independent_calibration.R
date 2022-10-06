183e689419800fe0e66835d0a441b93b8cdf4009
Sara Al-Assam <sarassam@gmail.com>
2022-10-06 16:46:32 +0100

    Addng function to calibrate single determination. Tested manually.

---
+++ R/independent_calibration.R
@@ -0,0 +1,24 @@
+#' Calibrates a single radiocarbon determination
+#'
+#' @param rc_determination A single observed radiocarbon determination (c14 age)
+#' @param rc_uncertainty The uncertainty of the radiocarbon determination
+#' @param calibration_data A dataframe which should contain one column entitled
+#' c14_age and one column entitled c14_sig
+#'
+#' @return A vector containing the probability for each calendar age in the
+#' calibration data
+#' @export
+#'
+CalibrateSingleDetermination <- function(
+    rc_determination,
+    rc_uncertainty,
+    calibration_data) {
+
+  c14_ages = calibration_data$c14_age
+  c14_sigs = calibration_data$c14_sig
+
+  probabilities <- stats::dnorm(
+    rc_determination, mean=c14_ages, sd=sqrt(c14_sigs^2 + rc_uncertainty^2))
+  probabilities <- probabilities/sum(probabilities)
+  return(probabilities)
+}
