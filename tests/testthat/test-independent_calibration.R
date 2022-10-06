f5c97a787b4e9a98e2e306beeb16a78d01a0d71e
Sara Al-Assam <sarassam@gmail.com>
2022-10-06 17:21:33 +0100

    Adding basic tests

--- tests/testthat/test-independent_calibration.R
+++ tests/testthat/test-independent_calibration.R
@@ -1,3 +1,17 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("single determination probabilities same length as calibration data", {
+  test_calibration_data = data.frame(
+    c14_age = c(2, 3, 4, 5, 6, 7, 8),
+    c14_sig = c(0.2, 0.1, 0.3, 0.3, 0.4, 0.5, 0.3)
+  )
+  probabilities = CalibrateSingleDetermination(4, 0.1, test_calibration_data)
+  expect_equal(length(probabilities), length(test_calibration_data$c14_age))
+})
+
+test_that("single determination probabilities sum to one", {
+  test_calibration_data = data.frame(
+    c14_age = c(2, 3, 4, 5, 6, 7, 8),
+    c14_sig = c(0.2, 0.1, 0.3, 0.3, 0.4, 0.5, 0.3)
+  )
+  probabilities = CalibrateSingleDetermination(4, 0.1, test_calibration_data)
+  expect_equal(sum(probabilities), 1)
 })
