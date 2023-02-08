46e1f2245a12a2f3f75676fde2806920fdf7fca5
Sara Al-Assam <sarassam@gmail.com>
2023-02-08 15:52:24 +0000

    Keep only first 3 columns for intcal20

--- data-raw/intcal20.R
+++ data-raw/intcal20.R
@@ -1,12 +1,10 @@
 
 library(usethis)
 
-intcal20 <- read.table("data-raw/intcal20.14c", sep = ",", header = FALSE, skip = 11)
+intcal20 <- read.csv("data-raw/intcal20.14c", header = FALSE, skip=11)[,1:3]
 names(intcal20) <- c(
   "calendar_age",
   "c14_age",
-  "c14_sig",
-  "Delta14C",
-  "DeltaSigma")
+  "c14_sig")
 
 usethis::use_data(intcal20, overwrite = TRUE)
