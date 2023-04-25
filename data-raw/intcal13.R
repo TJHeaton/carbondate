b9b9ab5e2d3124d27c541b7f72ce66512c18c1e4
Sara Al-Assam <sarassam@gmail.com>
2023-04-25 10:11:56 +0100

    Revert "Add the other intcal curves"
    This reverts commit 84e39608843b3d9c8dffb74f91a4bf3736343706.

--- data-raw/intcal13.R
+++
@@ -1,7 +0,0 @@
-intcal13 <- read.csv("data-raw/intcal13.14c", header = FALSE, skip=11)[,1:3]
-names(intcal13) <- c(
-  "calendar_age",
-  "c14_age",
-  "c14_sig")
-
-usethis::use_data(intcal13, overwrite = TRUE)
