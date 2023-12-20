4b589db5383ebf232555790178a28d133d63caff
Sara Al-Assam <sarassam@gmail.com>
2023-12-20 16:09:47 +0000

    Got basic CPP version working but not giving exactly correct results

--- experimenting/compare_fast_and_not_fast.R
+++ experimenting/compare_fast_and_not_fast.R
@@ -11,23 +11,23 @@ for (species in c("alces", "bison")) {
 
   set.seed(14)
   ppo_fast[[species]] <- PPcalibrate(
-    all_data[[species]]$c14_age, all_data[[species]]$c14_sig, intcal20, n_iter = 1e5, calendar_grid_resolution = 10, use_cpp = TRUE)
+    all_data[[species]]$c14_age, all_data[[species]]$c14_sig, intcal20, n_iter = 5e4, calendar_grid_resolution = 10, use_cpp = TRUE)
 
   set.seed(14)
   ppo_normal[[species]] <- PPcalibrate(
-    all_data[[species]]$c14_age, all_data[[species]]$c14_sig, intcal20, n_iter = 1e5, calendar_grid_resolution = 10, use_cpp = FALSE)
+    all_data[[species]]$c14_age, all_data[[species]]$c14_sig, intcal20, n_iter = 5e4, calendar_grid_resolution = 10, use_cpp = FALSE)
 
   layout.matrix <- matrix(c(1, 2, 3, 4, 5, 6, 7, 8), nrow = 4, ncol = 2)
   layout(mat = layout.matrix, heights = c(1, 1), widths = c(1, 1))
 
   PlotPosteriorMeanRate(ppo_fast[[species]])
-  title(paste("Fast version", species))
+  title(sub = paste("Fast version", species))
   PlotPosteriorChangePoints(ppo_fast[[species]], n_changes = c(1, 2, 3, 4))
   PlotPosteriorHeights(ppo_fast[[species]], n_changes = c(1, 2, 3, 4))
   PlotNumberOfInternalChanges(ppo_fast[[species]])
 
   PlotPosteriorMeanRate(ppo_normal[[species]])
-  title(paste("Original version", species))
+  title(sub = paste("Original version", species))
   PlotPosteriorChangePoints(ppo_normal[[species]], n_changes = c(1, 2, 3, 4))
   PlotPosteriorHeights(ppo_normal[[species]], n_changes = c(1, 2, 3, 4))
   PlotNumberOfInternalChanges(ppo_normal[[species]])
