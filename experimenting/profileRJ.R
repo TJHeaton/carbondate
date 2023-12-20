aaa5722e8d8b718a3965f345362e8558ce7d5af4
Sara Al-Assam <sarassam@gmail.com>
2023-12-20 09:51:27 +0000

    Update profiling script

--- experimenting/profileRJ.R
+++ experimenting/profileRJ.R
@@ -5,24 +5,11 @@ load_all()
 set.seed(14)
 
 file_pref <- "humans"
-cutoffages <- c(6000, 25000)
-data <- read.csv("experimenting/AnimalExtinctions/Humanc14Dates.csv", header = TRUE, na.strings = c("", "greater than"))
-keep <- which(data$X14C < cutoffages[2] & data$X14C > cutoffages[1])
-data <- data[keep,]
-removena <- which(is.na(data$X14C) | is.na(data$X1..Sigma))
-if(length(removena) != 0) { # Remove any values where 14C or sigma is NA
-  data <- data[-removena,]
-}
-
-rc_determinations <- data$X14C
-rc_sigmas <- rep(500, length(rc_determinations))# data$X1..Sigma
-
-rm(keep, data, removena)
 
 Rprof(paste0("experimenting/", file_pref, ".out"))
 
 Test_Output <- PPcalibrate(
-  rc_determinations, rc_sigmas, intcal20, n_iter = 1e4, n_thin = 10, calendar_grid_resolution = 10, use_fast = TRUE)
+  human$c14_age, human$c14_sig, intcal20, n_iter = 5e4, n_thin = 10, calendar_grid_resolution = 10, use_fast = TRUE)
 
 Rprof(NULL)
 
